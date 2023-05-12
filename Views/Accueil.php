de37b3bd746ba8270ed771b8688b364e9b923271
hsfalt <llkinako0410@gmail.com>
2023-05-12 10:32:11 +0200

    modifié :         Accueil.php 	modifié :         MaPage.php

--- Views/Accueil.php
+++ Views/Accueil.php
@@ -4,14 +4,10 @@
     <head>
         <link rel="stylesheet" type="text/css" href="../css/Accueil.css" /> 
         <link rel="icon" type="image/png" href="https://imgur.com/7NWBQEx.png">
-        <title>ToList</title>
+        <title>Accueil</title>
     </head>
 
     <body>
-        <header>
-            <?php
-             require_once("./Header.php");
-            ?>
-        </header>
+        <?php require_once("./Header.php");?>
     </body>
 </html>
--- Views/MaPage.php
+++ Views/MaPage.php
@@ -1,13 +1,13 @@
 <?php
 ?>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/Accueil.css" /> 
+        <link rel="icon" type="image/png" href="https://imgur.com/7NWBQEx.png">
+        <title>Ma page</title>
+    </head>
     <body>
-        <p>ma page</p>
-        <header>
-            <?php
-            $navbar=file_get_contents("elements/NavbarGuest.html");
-            echo $navbar;
-            ?>
-        </header>
+        <?php require_once("./Header.php");?>
+        <p>Des infos pertinentes</p>
     </body>
 </html>
