66f32582746be5e938b5e9f19ff666057f1d56db
hsfalt <llkinako0410@gmail.com>
2023-05-12 09:22:15 +0200

    nouveau fichier : Views/AccueilGuest.php 	supprimé :        Views/Accueil.php 	modifié :         index.php

---
+++ Views/AccueilGuest.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../css/Accueil.css" /> 
+        <link rel="icon" type="image/png" href="https://imgur.com/7NWBQEx.png">
+        <title>ToList</title>
+    </head>
+
+    <body>
+        <header>
+            <navbar>
+            <a id="logo" href="http://192.168.8.212:8000/Views/Accueil.php"><img src="https://imgur.com/7NWBQEx.png"></a>
+            <a href="Connexion.php">Connexion</a>
+            </navbar>
+        </header>
+    </body>
+</html>
