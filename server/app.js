abdbabd5fe0ba081a2dc0d21675120fc442837e9
Amice Baptiste <amice.bap@gmail.com>
2023-09-28 12:30:05 +0200

    routes file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-const router = require('./src/routes/routes');
+const router = require('./src/routes/global.routes');
 
 app.use(bodyParser.json());
 app.use(
