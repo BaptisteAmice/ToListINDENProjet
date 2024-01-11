8de2138b7bd8ea07348005bf2e2c9292f0d6f3a3
Amice Baptiste <amice.bap@gmail.com>
2024-01-11 18:58:17 +0100

    update affichage liste titles

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, lastValueFrom } from 'rxjs';
+import { ApiHelperService } from '../services/api-helper.service';
+import { PopupService } from '../popup/popup.service';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,24 +11,87 @@ import { Observable } from 'rxjs';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private http: HttpClient) { }
 
-  items: string[] = [];
+  dataSource: any = [];
+
+  searchValue: string = '';
+  fullTitleList: any = [];
+  searchOptions: string[] = ['_id','title'];
+  searchParam: string = 'title';
+
+  constructor(
+      private api: ApiHelperService,
+      private http: HttpClient,
+      private popupService: PopupService,
+  ) {}
 
   ngOnInit(): void {
-    this.getTitles().subscribe({
-      next: (response) => {
-        // Assurez-vous que la réponse contient un tableau d'objets avec la propriété "name"
-        this.items = response.map(item => item.title);
-      },
-      error: (error) => {
-        console.error('Erreur lors de la récupération des données :', error);
-      }
-    });
+    this.updateDataSource();
   }
 
-  getTitles(): Observable<any[]> {
-    return this.http.get<any[]>(`http://localhost:3000/titles`);
+  updateDataSource(): void {
+    const request: Observable<any> = this.http.get(environment.api + '/titles', { observe: 'response' });
+    next : lastValueFrom(request).then(response => {
+      this.fullTitleList = response.body;
+      this.dataSource = this.fullTitleList;
+    },
+      error => {
+        this.popupService.openPopup("Can't get titles : "+ error.message,"Error "+error.status);
+      });
+  }
+
+
+  add(): void {
+    /*
+    const pseudo: string = (document.getElementById('pseudo') as HTMLInputElement).value;
+    const mail: string = (document.getElementById('mail') as HTMLInputElement).value;
+    //current date (format : YYYY-MM-DD)
+    const inscription: string = new Date().toISOString().slice(0,10);
+    //generate a random password
+    const password: string = Math.random().toString(30).slice(-8);
+
+    if(pseudo == '' || mail == ''){
+      this.popupService.openPopup("Please fill all fields");
+      return;
+    }
+
+    try {
+      this.api.post({endpoint: '/users', data: { pseudo, mail, inscription, password }}).then(response => {},
+        error => this.popupService.openPopup("Can't add user : "+ error.message,"Error "+error.status))
+        .finally(() => {
+          //update the list (we can't just simulate the update because we need the generated id)
+          this.updateDataSource();
+          this.popupService.openPopup("User added with temporary password ( please change it later ) : " + password);
+        });
+    } catch (error) {
+      console.log(error);
+    }*/
+  }
+
+  delete(id: string): void{
+    this.api.delete({endpoint: '/titles/'+id}).then(response => {},
+      error => this.popupService.openPopup("Can't delete title : "+ error.message,"Error "+error.status))
+      .finally(() => {
+        //update the list (simulate the update to avoid a new request)
+        this.fullTitleList = this.fullTitleList.filter((title: any) => title._id != id);
+        this.dataSource = this.fullTitleList;
+      });
+  }
+
+  updateSearch(): void{
+    //filter the list
+    if(this.searchValue != ''){
+      this.dataSource = this.fullTitleList.filter((title: any) => {
+        return title[this.searchParam].toString().toLowerCase().includes(this.searchValue.toLowerCase());
+      });
+    } else {
+      this.dataSource = this.fullTitleList;
+    }
+  }
+
+  setSearchParam(param: string): void{
+    this.searchParam = param;
+    this.updateSearch();
   }
 
 }
